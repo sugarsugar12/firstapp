87203d3faaa7cdb4c7d5d651f527bd08e02d1152
sugarsugar12 <146008738+sugarsugar12@users.noreply.github.com>
2024-04-23 14:01:18 +0900

    first commit

---
+++ PostController.java
@@ -0,0 +1,41 @@
+package in.techcamp.firstapp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class PostController {
+    private final PostRepository postRepository;
+    @GetMapping("/hello")
+    public String showHello(Model model) {
+        var sampleText = "サンプルテキスト";
+        model.addAttribute("sampleText", sampleText);
+        return "hello";
+    }
+
+    @GetMapping
+    public String showList(Model model) {
+        var postList = postRepository.findAll();
+        model.addAttribute("postList", postList);
+        return "index";
+    }
+
+    @GetMapping("/postForm")
+    public String showPostForm(@ModelAttribute("postForm") PostForm form){
+        return "postForm";
+    }
+
+    @PostMapping("/posts")
+    public String savePost(PostForm form){
+        postRepository.insert(form.getMemo());
+        return "redirect:/";
+    }
+}
